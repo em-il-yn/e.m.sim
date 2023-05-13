6204cabbc647d4e9ac9a343d92a08d0fa3e051c2
em-il-yn <63993946+em-il-yn@users.noreply.github.com>
2023-05-12 23:56:58 -0700

    Designing

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
 const App = () => (
   <html>
-    <head>
-      <title>Emilyn Sim</title>
-    </head>
-    <body>
-      <header>
-        <div id="header-container">
+    <div id="site-wrapper">
+      <head>
+        <title>Emilyn Sim</title>
+      </head>
+      <body>
+        <header id="header" data-current-styles="navCenter">
           <h1>Emilyn Sim</h1>
           <nav>
             <ul>
-              <li>
-                <a href="https://www.linkedin.com/in/emilyn-sim/">LinkedIn</a>
-              </li>
-              <li>
-                <a href="/resources/SimEmilyn_Resume.pdf"> Resume </a>
-              </li>
+              <li><a href="#">About Me</a></li>
+              <li><a href="#">Technical Experience</a></li>
+              <li><a href="#">Communications Experience</a></li>
             </ul>
           </nav>
-        </div>
-      </header>
-      <main>
-        <section>
-          <div className="gallery">
-            <img src="/resources/headshot.jpg" alt="current headshot" style={{width: '225px', height: '300px'}} />
-          </div>
-          <article>
-            <h2> About Me </h2>
-            <p> I am currently a Bachelor of Computer Science student at the University of British Columbia. 
-              I transitioned to computer science from communications, so I have a background in organizational communication. 
-              Check out my LinkedIn for an overview of my experience.</p>
-          </article>
-          <article>
-            <h2> Contact </h2>
-            <p> Vancouver BC | emmysim16@gmail.com | (250) 463 3294 </p>
-          </article>
-        </section>
-      </main>
-    </body>
+        </header>
+        <main>
+          <section>
+            <div className="gallery">
+              <img src="/resources/headshot.jpg" alt="current headshot" style={{ width: '225px', height: '300px' }} />
+            </div>
+            <article>
+              <h2> About Me </h2>
+              <p> I am currently a Bachelor of Computer Science student at the University of British Columbia.
+                I transitioned to computer science from communications, so I have a background in organizational communication.
+                Check out my LinkedIn for an overview of my experience.</p>
+            </article>
+            <article>
+              <h2> Contact </h2>
+              <p> Vancouver BC | emmysim16@gmail.com | (250) 463 3294 </p>
+            </article>
+          </section>
+        </main>
+      </body>
+    </div>
   </html>
 );
 
