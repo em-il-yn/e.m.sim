0d7b5586fe9a1ea94979bd514cabfb69cbc4e3e0
em-il-yn <63993946+em-il-yn@users.noreply.github.com>
2023-05-18 19:47:11 -0700

    Updating Technical Page

--- src/pages/Technical.js
+++ src/pages/Technical.js
@@ -1,16 +1,43 @@
-// import Cursor from "./../Events/cursorEvent";
+
 // const Technical = () => {
-//   return (
-//   <html>
-//     <head>
-//       <title>Technical Experience</title>
-//     </head>
-//     <header>
-//       <p class="pagetop">Emilyn Sim</p>
-//     </header>
-//     <body>
-//       <Cursor />
-//       <section class="column">
+//     return (
+//         <html>
+//             <head>
+//                 <title>Technical Experience</title>
+//             </head>
+//             <body>
+//                 <textCircle>
+//                     Software Developer Intern Jul 2021 – Mar 2022
+//                     Faster Than Light Computing
+//                     ▪ Used BASH scripting to automate the integration of a NAC into the
+//                     deployment of company services, enabling remote configuration of
+//                     switches and VLANs and streamlining the deployment process.
+//                     ▪ Created a solution for remote tech support to approve devices to different
+//                     VLANs and configure switches, improving support efficiency and reducing
+//                     manual effort.
+//                     ▪ Researched and evaluated various methods of authorization into VLANs,
+//                     optimizing network access control and improving network security.
+//                     ▪ Worked under the supervision of a senior developer and network
+//                     administrator, gaining knowledge in network administration, software
+//                     development, and team collaboration.
+//                 </textCircle>
+//                 <textCircle>
+//                     WearWise Application Feb 2023 – Apr 2023
+//                     A Cost Tracking Application
+//                     ▪ Developed Java-based functionality to allow users to track clothing cost and wear count over time,
+//                     generating data on cost per wear for clothing and brands.
+//                     ▪ Designed and implemented features using object-oriented programming principles.
+//                     ▪ Troubleshot issues in the codebase using a debugger and tested classes with JUnit tests,
+//                     demonstrating proficiency in debugging techniques and test-driven development (TDD).
+//                     ▪ Utilized JSON parsing skills to model data presented in JSON format.
+//                 </textCircle>
+//                 <textCircle>
+//                     Hackathon: cmd-f by nwhacks Mar 2023
+//                     UML Diagram Study Tool
+//                     ▪ Developed an interactive study tool for computer science students using ReactJS
+//                     ▪ Worked collaboratively with a team to design and implement a user-friendly interface
+//                 </textCircle>
+//                 {/* <section class="column">
 //         <section class="row">
 //           <section class="column">
 //             <h1>Emilyn Sim</h1>
@@ -24,16 +51,10 @@
 //             <a class="links" href="https://github.com/em-il-yn">GitHub</a>
 //           </section>
 //         </section>
-//       </section>
-//     </body>
-//     <footer>
-//       <section class="contact">
-//         <h2> Contact </h2>
-//         <p> Vancouver BC | esim01@student.ubc.ca | 250 463 3294 </p>
-//       </section>
-//     </footer>
-//   </html>
-// )
-//   }
+//       </section> */}
+//             </body>
+//         </html>
+//     )
+// }
 
 // export default Technical;
