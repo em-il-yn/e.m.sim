ecda2fb1ef8b95746b8de10cde845f4d8b636912
em-il-yn <63993946+em-il-yn@users.noreply.github.com>
2023-05-23 14:10:37 -0700

    deployement

--- src/index.js
+++ src/index.js
@@ -2,22 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-    },
-  ]);
-
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
+import { BrowserRouter } from 'react-router-dom'
 
 
+ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter basename={"https://em-il-yn.github.io/emilynsim.ubc/"}><App /></BrowserRouter>)
