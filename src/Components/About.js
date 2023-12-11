ce1c4bfda6f8863de56f2616a607183539348dc1
em-il-yn <63993946+em-il-yn@users.noreply.github.com>
2023-12-10 23:19:47 -0800

    Typo Fixed

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,7 @@ const About = () => {
                     <section class="columnEvil" style={{alignSelf: 'center'}}>
                         <h1>E.M. Sim</h1>
                         <h2 style={{ alignSelf: 'left' }}>UBC Computer Science</h2>
-                        <p>I am a UBC Computer Science student with a professional background in strategic communications. I have xperience with Java, JavaScript, BASH, HTML, CSS, Racket, C++, C, PHP, and R. Explore my website for samples of my work.</p>
+                        <p>I am a UBC Computer Science student with a professional background in strategic communications. I have experience with Java, JavaScript, BASH, HTML, CSS, Racket, C++, C, PHP, and R. Explore my website for samples of my work.</p>
                     </section>
                     <section class="column">
                         <a class="links" href={require('./../Resources/SimEM_Resume.pdf')} alt="Resume">Resume</a>
